571b0f5c4f6e169795f5892d50dcd6962725e7a0
Ianyin21 <88044808+Ianyin21@users.noreply.github.com>
2021-07-30 16:56:21 +0100

    Add files via upload

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,20 @@
+<<<<<<< HEAD
+from django.contrib import admin
+from rango.models import Category, Page, UserProfile
+
+# Register your models here.
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
+=======
 from django.contrib import admin
 
 # Register your models here.
+>>>>>>> ddb9f11ee539b3992753f11402017d470cdc5e89
--- rango/apps.py
+++ rango/apps.py
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 from django.apps import AppConfig
 
 
 class RangoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'rango'
+=======
+from django.apps import AppConfig
+
+
+class RangoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'rango'
+>>>>>>> ddb9f11ee539b3992753f11402017d470cdc5e89
